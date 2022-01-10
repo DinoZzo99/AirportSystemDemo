c63d1965d956beaffe325dc98d617ef352992112
DinoZzo99 <66387612+DinoZzo99@users.noreply.github.com>
2022-01-10 15:38:07 +0100

    Add files via upload

---
+++ Aviation/AviationBlazorServer/Services/AirportService.cs
@@ -0,0 +1,30 @@
+﻿using DomainModel;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace AviationBlazorServer.Services
+{
+    public class AirportService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string BaseApiUrl = "https://localhost:44342/api";
+        public AirportService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<List<Airport>> GetAirports()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Airport>>(BaseApiUrl+"/Airport");
+        }
+        public async Task<List<Airport>> GetAirportsDescending()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Airport>>(BaseApiUrl+"/Airport/Descending");
+        }
+        public async Task<List<Country>> GetCountries()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Country>>(BaseApiUrl + "/Country");
+        }
+    }
+}
